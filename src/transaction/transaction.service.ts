4a462571a4001358841c9f16c0f3e0cbd360df09
Boby <boblaveyou@gmail.com>
2026-02-20 07:51:01 +0700

    update transaction

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -3,15 +3,33 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Transaction } from './entities/transaction.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Topic } from 'src/topic/entities/topic.entity';
+import { json } from 'sequelize';
 
 @Injectable()
 export class TransactionService {
   constructor(
     @InjectModel(Transaction)
     private readonly transactionModel: typeof Transaction,
+    @InjectModel(User)
+    private readonly userModel: typeof User,
+    @InjectModel(Topic)
+    private readonly topicModel: typeof Topic,
   ) {}
 
   create(createTransactionDto: CreateTransactionDto) {
+    const { userId, topicId } = createTransactionDto;
+    const user = this.userModel.findOne({ where: { id: userId } });
+    if (!user) {
+      return json({ error: 'User not found' });
+    }
+
+    const topic = this.topicModel.findOne({ where: { id: topicId } });
+    if (!topic) {
+      return json({ error: 'Topic not found' });
+    }
+
     return this.transactionModel.create({
       transactionId: createTransactionDto.transactionId,
       userId: createTransactionDto.userId,
